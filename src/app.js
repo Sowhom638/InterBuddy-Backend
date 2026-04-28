423186432213fa6d1f64f40f585d7c6dc2dc1ebd
Sowhom638 <sowhom.ghosh.22@aot.edu.in>
2026-04-28 10:44:46 +0530

    removing vapi from the app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
-const cors = require("cors"); 
+const cors = require("cors");
 require("dotenv").config();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.FRONTEND,
-    credentials: true
-}))
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 const authRouter = require("./routes/auth.route");
 const interviewRouter = require("./routes/interview.route");
-const vapiRouter = require("./routes/vapi.route");
 app.use("/api/auth", authRouter);
 app.use("/api/interview", interviewRouter);
-app.use("/api/vapi", vapiRouter);
 module.exports = app;
