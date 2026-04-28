183cb65b3cab842cce80426ea4e71a684f7b1fed
Sowhom638 <sowhom.ghosh.22@aot.edu.in>
2026-04-28 10:26:51 +0530

    Removed vapi from backend

--- src/routes/vapi.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const vapiRouter = express.Router();
-const { authUser } = require("../middlewares/auth.middleware");
-const { generateReportController } = require("../controllers/vapi.controller");
-
-vapiRouter.post("/", authUser, generateReportController);
-
-module.exports = vapiRouter;
--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -77,37 +77,6 @@ async function generateInterviewReport({
   return JSON.parse(response.text);
 }
 
-async function generateMockInterviewText({
-  role,
-  level,
-  techstack,
-  type,
-  amount
-}) {
-  const prompt = `Prepare questions for a job interview.
-The job role is ${role}.
-The job experience level is ${level}.
-The tech stack used in the job is: ${techstack}.
-The focus between behavioural and technical questions should lean towards: ${type}.
-The amount of questions required is: ${amount}.
-Please return only the questions, without any additional text.
-The questions are going to be read by a voice assistant so do not use "/" or "*" or any special characters.
-Return the questions formatted like this:
-["Question 1", "Question 2", "Question 3"]
-
-Thank you! <3`;
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: prompt,
-    config: {
-      responseMimeType: "application/json",
-    },
-  });
-  console.log(response.text);
-  return JSON.parse(response.text);
-}
-
 module.exports = {
-  generateInterviewReport,
-  generateMockInterviewText,
+  generateInterviewReport
 };
